2b08ddbeb2ba1a2d96eab809183936d3d0dea1ed
Grace Benkelman <gbenkelman@college.harvard.edu>
2024-09-10 17:25:52 -0400

    updated math response and week2md

--- docs/math-response-time.js
+++ docs/math-response-time.js
@@ -1,17 +1,38 @@
 //instruction alert
 alert("In this experiment we will measure your response time. You will be shown a series of simple math equations. Answer these equations as quickly and accurately as you can.");
 
-//first popup question and response propmpt
+//Start timer 
+let startTime1 = Date.now();
+//ask initial question
 let response1 = prompt("What is 1 + 2?");
-alert("You answered" + response1 + "!");
+//end timer
+let endTime1 = Date.now();
+//take end time minus start time and convert from milliseconds into seconds
+let reactionTime1 = ((endTime1 - startTime1) / 1000);
+//closing alert
+alert("You answered " + response1 + " in" + reactionTime1 + " seconds!");
 
-//second popup question and response prompt
+//Start timer 
+let startTime2 = Date.now();
+//ask initial question
 let response2 = prompt("What is 4 + 2?");
-alert("You answered" + response2 + "!");
+//end timer
+let endTime2 = Date.now();
+//take end time minus start time and convert from milliseconds into seconds
+let reactionTime2 = ((endTime2 - startTime2) / 1000);
+//closing alert
+alert("You answered " + response2 + " in" + reactionTime2 + " seconds!");
 
-//third popup question and response prompt
+//Start timer 
+let startTime3 = Date.now();
+//ask initial question
 let response3 = prompt("What is 6 + 4?");
-alert("You answered" + response3 + "!");
+//end timer
+let endTime3 = Date.now();
+//take end time minus start time and convert from milliseconds into seconds
+let reactionTime3 = ((endTime3 - startTime3) / 1000);
+//closing alert
+alert("You answered " + response3 + " in" + reactionTime3 + " seconds!");
 
 //end message!
 alert("Thank you for your participation!");
--- outside-resources/week2.md
+++ outside-resources/week2.md
@@ -7,6 +7,9 @@ _Record any prompts you put into an AI program this week to help with material i
 how do I use javascript to get a console output changing all the letters in a title to capital?
     if I have already created courseName to be my title, how do I incorporate that?
 how to make my alert show up in javascript?
+how do I use the date.now() command to record reaction time in an alert in javascript?
+
+
 
 
 
