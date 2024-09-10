78e668c5463a32db83c867cf216c503cb8a8f2a2
Grace Benkelman <gbenkelman@college.harvard.edu>
2024-09-10 17:10:30 -0400

    math questions

--- docs/math-response-time.js
+++ docs/math-response-time.js
@@ -1,14 +1,19 @@
+//instruction alert
 alert("In this experiment we will measure your response time. You will be shown a series of simple math equations. Answer these equations as quickly and accurately as you can.");
 
+//first popup question and response propmpt
 let response1 = prompt("What is 1 + 2?");
 alert("You answered" + response1 + "!");
 
+//second popup question and response prompt
 let response2 = prompt("What is 4 + 2?");
 alert("You answered" + response2 + "!");
 
+//third popup question and response prompt
 let response3 = prompt("What is 6 + 4?");
 alert("You answered" + response3 + "!");
 
+//end message!
 alert("Thank you for your participation!");
 
 
