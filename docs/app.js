51f406c54d011da1ec5c13d2aea20aa1f3388ec7
Grace Benkelman <gbenkelman@college.harvard.edu>
2024-09-10 15:31:02 -0400

    adding OR folder

---
+++ docs/app.js
@@ -0,0 +1,37 @@
+//define a variable to hold experiment name
+let experiment = 'Grace';
+console.log(typeof experiment);
+let trialCountMax = 20;
+// TODO: randomize colors
+let colors = ['red', 'green', 'blue'];
+
+alert('Welcome to the ' + experiment + ' experiment!');
+
+trialCountMax = 40;
+//console.log(charAt(0));
+
+
+// at halfway point we will display pause screen
+let halfWayCount = trialCountMax / 2;
+
+console.log(halfWayCount);
+
+/* 
+multi
+line
+comment 
+*/
+
+let correct = true;
+console.log(typeof correct);
+
+// An Array of Numbers
+let scores = [75.5, 91, 91.6, 92.3, 45.5];
+
+// An Array of Strings
+let words = ['pear', 'mouse', 'red', 'igloo'];
+
+// An Array of mixed data types, including a sub-array
+let misc = [90, 'green', 45.5, ['a', 'b', 'c']];
+
+
