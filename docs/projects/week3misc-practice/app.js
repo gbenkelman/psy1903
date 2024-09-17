a184a5cee362887f7fa54a615fef3ef6092be330
Grace Benkelman <gbenkelman@college.harvard.edu>
2024-09-17 17:06:52 -0400

    added question 3!

--- docs/projects/week3misc-practice/app.js
+++ docs/projects/week3misc-practice/app.js
@@ -1,4 +1,4 @@
-let randomNumber1 = Math.floor(Math.random() * 10) + 1;
+/* let randomNumber1 = Math.floor(Math.random() * 10) + 1;
 let randomNumber2 = Math.floor(Math.random() * 10) + 1;
 let answer = randomNumber1 + randomNumber2;
 
@@ -25,4 +25,12 @@ if (age >= 12 && age < 18) {
 }
 if (age >= 18) {
     alert('Adult');
+} */
+
+let whole = prompt('Type a whole number');
+if (whole % 2 == 0) {
+    alert('Even!');
+}
+if (whole % 2 != 0) {
+    alert('Odd!');
 }
