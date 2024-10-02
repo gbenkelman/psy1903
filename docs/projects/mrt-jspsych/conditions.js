bda8d8dac777eb9294966ee4bd78575ea6c7739d
Grace Benkelman <gbenkelman@college.harvard.edu>
2024-10-02 17:59:13 -0400

    question 2

--- docs/projects/mrt-jspsych/conditions.js
+++ docs/projects/mrt-jspsych/conditions.js
@@ -1,1 +1,24 @@
 let jsPsych = initJsPsych();
+
+let timeline = [];
+
+// Welcome Screen
+let welcomeTrial = {
+    type: jsPsychHtmlKeyboardResponse,
+    stimulus: `
+    <h1>Welcome to the Math Response Time Task!</h1>
+    <p>In this experiment, you will solve simple math problems as quickly and accurately as possible.</p>
+    <p>Press SPACE to begin.</p>`,
+    choices: [' '],
+};
+timeline.push(welcomeTrial);
+
+// Create Random Conditions
+let conditions = [];
+for (let i = 0; i < 3; i++) {
+    let num1 = Math.floor(Math.random() * 10) + 1;
+    let num2 = Math.floor(Math.random() * 10) + 1;
+    let answer = num1 + num2;
+    conditions.push({ num1: num1, num2: num2, answer: answer });
+};
+
