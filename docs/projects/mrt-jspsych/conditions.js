ce8573277a693bbf48b3c0460e760f8e89b0f250
Grace Benkelman <gbenkelman@college.harvard.edu>
2024-10-04 14:09:25 -0400

    question 4

--- docs/projects/mrt-jspsych/conditions.js
+++ docs/projects/mrt-jspsych/conditions.js
@@ -1,8 +1,9 @@
 let jsPsych = initJsPsych();
 
+//create timeline
 let timeline = [];
 
-// Welcome Screen
+//first part of experiment, welcome
 let welcomeTrial = {
     type: jsPsychHtmlKeyboardResponse,
     stimulus: `
@@ -13,7 +14,6 @@ let welcomeTrial = {
 };
 timeline.push(welcomeTrial);
 
-// Create Random Conditions
 let conditions = [];
 for (let i = 0; i < 3; i++) {
     let num1 = Math.floor(Math.random() * 10) + 1;
@@ -24,3 +24,51 @@ for (let i = 0; i < 3; i++) {
 
 console.log(conditions);
 
+conditions = jsPsych.randomization.repeat(conditions, 1);
+
+for (let condition of conditions) {
+    let conditionTrial = {
+        type: jsPsychSurveyHtmlForm,
+        html: `<p>${condition.num1} + ${condition.num2} = </p>
+               <input name="response" type="number" required></input>`,
+        data: {
+            collect: true,
+        },
+        on_finish: function (data) {
+            data.num1 = condition.num1;
+            data.num2 = condition.num2;
+            data.answer = condition.answer;
+            //correct or  not?
+            let userResponse = data.response.response;
+            if (userResponse == condition.answer) {
+                data.correct = true;
+            }
+            else {
+                data.correct = false;
+            }
+        }
+    };
+    timeline.push(conditionTrial);
+}
+
+
+let debriefTrial = {
+    type: jsPsychHtmlKeyboardResponse,
+    stimulus: `
+    <h1>Thank you for participating!</h1> 
+    <p>You can close this tab.</p>
+    `,
+    choices: ['NO KEYS'],
+    on_start: function () {
+        let data = jsPsych.data
+            .get()
+            .filter({ collect: true })
+            .ignore(['response', 'stimulus', 'trial_type', 'trial_index', 'plugin_version', 'collect'])
+            .csv();
+        console.log(data);
+    }
+};
+
+timeline.push(debriefTrial);
+
+jsPsych.run(timeline);
