41b1f786cf8f76dffc33435a686af3f342208c97
Grace Benkelman <gbenkelman@college.harvard.edu>
2024-10-17 20:23:19 -0400

    filter numbers

--- docs/projects/week4-tast-set/app.js
+++ docs/projects/week4-tast-set/app.js
@@ -48,7 +48,7 @@ function getLongestWord(words) {
 
 console.log(getLongestWord(words));
 */
-
+/*
 function getOddNumbers(numbers) {
 
     let oddNumbers = [];
@@ -65,3 +65,30 @@ function getOddNumbers(numbers) {
 // ex:
 console.log(getOddNumbers([1, 2, 3, 4, 5])); // Expected output: [1, 3, 5]
 console.log(getOddNumbers([12, 45, 10, 11, 61])); // 
+
+*/
+
+function filterNumbers(numbers, evenOrOdd) {
+    let numbers1 = [];
+
+    for (let i = 0; i < numbers.length; i++) {
+        if (evenOrOdd === 'even') {
+            if (numbers[i] % 2 === 0) {
+                numbers1.push(numbers[i]);
+            }
+        } else if (evenOrOdd === 'odd') {
+            if (numbers[i] % 2 !== 0) {
+                numbers1.push(numbers[i]);
+            }
+        }
+    }
+
+    return numbers1;
+}
+
+// Ex:
+console.log(filterNumbers([1, 2, 3, 4, 5], 'even')); // Expected output: [2, 4]
+console.log(filterNumbers([1, 2, 3, 4, 5], 'odd')); // Expected output: [1, 3, 5]
+
+console.log(filterNumbers([45, 10, 11, 61], 'even')); // Expected output: [10]
+console.log(filterNumbers([45, 10, 11, 61], 'odd')); // Expected output: [45, 11, 61]
